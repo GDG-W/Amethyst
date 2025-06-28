fcbde34774319278f0dd4f4ca0928be4f92c6a00
TemitopeAsama <temitopeasama@gmail.com>
2025-06-29 00:36:07 +0100

    chore: testing pre-commit hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import type { Metadata } from "next";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -19,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
