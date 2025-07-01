67187639d27fe24b8dbf309323bb1969cb835017
fasina-ayomikun <ayomikunfasina@yahoo.com>
2025-07-01 14:46:04 +0100

    removes all sizes instance in the button component

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -4,28 +4,21 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
   loading?: boolean;
   variant?: "primary" | "secondary" | "link";
-  size?: "large" | "normal" | "full";
 };
-const Button = ({ children, disabled, variant, size, loading = false, ...rest }: ButtonProps) => {
+const Button = ({ children, disabled, variant, loading = false, ...rest }: ButtonProps) => {
   const buttonStyles = {
     variants: {
       primary: "bg-away-base text-white ",
       secondary: "bg-away-base/10 border border-away-base text-away-base",
       link: "bg-transparent text-away-base underline underline-offset-2",
     },
-    sizes: {
-      large: "w-full sm:w-40",
-      full: " w-full",
-      normal: "w-full sm:w-fit",
-    },
   };
   const buttonVariant = variant || "primary";
-  const buttonSize = size || "normal";
   const isDisabled = loading || disabled;
   return (
     <button
       disabled={isDisabled}
-      className={`flex items-center justify-center gap-2 px-3 py-3 min-w-30 rounded-full capitalize hover:opacity-80 disabled:bg-bg-soft-200 disabled:text-white disabled:cursor-not-allowed ${buttonStyles.variants[buttonVariant]} ${buttonStyles.sizes[buttonSize]}
+      className={`flex items-center justify-center gap-2 px-3 py-3 w-full min-w-30 rounded-full capitalize hover:opacity-80 disabled:bg-bg-soft-200 disabled:text-white disabled:cursor-not-allowed ${buttonStyles.variants[buttonVariant]}
       `}
       {...rest}
     >
