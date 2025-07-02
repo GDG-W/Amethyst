24b178c006fd6a4f5b2d273fa67f09349dfebff2
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-07-02 01:52:48 -0700

    feat(header): refactor header component and update close icon implementation

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import HeaderComponent from "@/components/header/header";
+import HeaderComponent from "@/components/header";
 const page = () => {
   return <HeaderComponent />;
 };
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,8 +2,8 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 
-import LogoIcon from "../icons/LogoIcon";
-import CloseIcon from "../icons/CloseIcon";
+import LogoIcon from "../icons/logo";
+import CloseIcon from "../icons/close-icon";
 
 const HeaderComponent = () => {
   const router = useRouter();
@@ -15,7 +15,7 @@ const HeaderComponent = () => {
     <header className='bg-[#FFFAEB] h-screen p-6 sm:p-8 md:p-12'>
       <div className='flex justify-center bg-[#FFFFFF] border border-[#EBEBEB] px-5 py-4 md:px-10 md:py-5 max-w-3xl mx-auto rounded-full'>
         <div className='flex justify-between items-center w-full'>
-          <LogoIcon />
+          <LogoIcon className='w-36 md:w-40 xl:w-48' />
           <button
             onClick={handleClose}
             className='flex items-center gap-1 text-[#111111] text-base hover:text-gray-600 focus:text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 font-normal transition-colors cursor-pointer'
