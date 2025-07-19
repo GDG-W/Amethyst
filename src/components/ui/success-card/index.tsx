0ef4c8215e61794a70409f36d621ab65425bcd7e
fasina-ayomikun <ayomikunfasina@yahoo.com>
2025-07-19 11:30:13 +0100

    feat(ui): added a fixed width to card on desktop view

--- src/components/ui/success-card/index.tsx
+++ src/components/ui/success-card/index.tsx
@@ -14,7 +14,7 @@ const SuccessCard = ({ title, summary }: { title: string; summary?: string }) =>
       <main
         role='status'
         aria-live='polite'
-        className='w-full max-w-96 min-w-60 bg-white-0 border border-stroke-soft-200 rounded-md px-7 py-10 flex flex-col gap-3'
+        className='w-full md:w-96 max-w-96 min-w-60 bg-white-0 border border-stroke-soft-200 rounded-md px-7 py-10 flex flex-col gap-3'
       >
         <ConfettiIcon />
         <h4 className='heading-4 capitalize font-semibold text-white-0'>{title}</h4>
