13540952d89ee230a30879dca8ead80e2cededb5
fasina-ayomikun <ayomikunfasina@yahoo.com>
2025-07-20 08:47:09 +0100

    feat(ui): removed the main tag and replaced with a div, used the correct color code

--- src/components/ui/success-card/index.tsx
+++ src/components/ui/success-card/index.tsx
@@ -11,13 +11,9 @@ const SuccessCard = ({ title, summary }: { title: string; summary?: string }) =>
   const navigator = useRouter();
   return (
     <section className='w-full h-full flex items-center justify-center bg-transparent px-9 py-15'>
-      <main
-        role='status'
-        aria-live='polite'
-        className='w-full md:w-96 max-w-96 min-w-60 bg-white-0 border border-stroke-soft-200 rounded-md px-7 py-10 flex flex-col gap-3'
-      >
+      <div className='w-full  max-w-96 min-w-60 bg-bg-white-0 border border-stroke-soft-200 rounded-md px-7 py-10 flex flex-col gap-3'>
         <ConfettiIcon />
-        <h4 className='heading-4 capitalize font-semibold text-white-0'>{title}</h4>
+        <h4 className='heading-4 capitalize font-semibold text-black'>{title}</h4>
         {summary && <p className='text-text-sub-600 label-3'>{summary}</p>}
         <div className='flex flex-col gap-2 mt-3'>
           <Button
@@ -30,7 +26,7 @@ const SuccessCard = ({ title, summary }: { title: string; summary?: string }) =>
           </Button>
           <Button variant='secondary'>buy more</Button>
         </div>
-      </main>
+      </div>
     </section>
   );
 };
