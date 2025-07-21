ad675ad1559cf5385c28d05fda041ab2362fa143
DrPrime01 <drprime.dev@gmail.com>
2025-07-21 12:41:06 +0100

    chore: implemented the checkbox component ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
+import Checkbox from "@/components/form/checkbox";
 
 export default function Home() {
+  const [checked, setChecked] = useState(false);
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
@@ -16,7 +20,12 @@ export default function Home() {
           </li>
           <li className='tracking-[-.01em]'>Save and see your changes instantly.</li>
         </ol>
-
+        <Checkbox
+          name='approve'
+          label='This ticket belongs to me'
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+        />
         <div className='flex gap-4 items-center flex-col sm:flex-row'>
           <a
             className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto'
