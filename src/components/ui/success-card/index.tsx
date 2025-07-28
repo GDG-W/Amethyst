8fad043e5f339ee8b082c167026dfcb1b6dadeac
TemitopeAsama <temitopeasama@gmail.com>
2025-07-28 10:37:10 +0100

    chore: updated typography

--- src/components/ui/success-card/index.tsx
+++ src/components/ui/success-card/index.tsx
@@ -13,8 +13,8 @@ const SuccessCard = ({ title, summary }: { title: string; summary?: string }) =>
     <div className='w-full h-full flex items-center justify-center bg-transparent px-9 py-15'>
       <div className='w-full max-w-[410px] min-w-60 bg-white-0 border border-soft-200 rounded-md px-7 py-10 flex flex-col gap-3'>
         <ConfettiIcon />
-        <h4 className='heading-4 capitalize font-semibold text-black'>{title}</h4>
-        {summary && <p className='text-text-sub-600 label-3'>{summary}</p>}
+        <h4 className='heading-5 md:heading-4 capitalize font-semibold text-black'>{title}</h4>
+        {summary && <p className='text-text-sub-600 label-4 md:label-3'>{summary}</p>}
         <div className='flex flex-col gap-2 mt-3'>
           <Button
             variant='primary'
