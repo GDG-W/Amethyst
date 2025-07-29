17c24e5e7f369585c4bbadbdd1a87abde26cb84a
Oluwatobiju Judah <tobijudah@gmail.com>
2025-07-29 12:32:08 +0100

    fix: update Link component styles in header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -7,12 +7,12 @@ import Logo from "@/components/icons/logo";
 
 const Header = () => {
   return (
-    <header className='flex justify-center bg-white border border-bg-surface-800 px-5 py-4 md:px-10 md:py-5 max-w-[800px] mx-auto rounded-full'>
-      <nav className='flex justify-between items-center w-full'>
+    <header className='border-bg-surface-800 mx-auto flex max-w-[800px] justify-center rounded-full border bg-white px-5 py-4 md:px-10 md:py-5'>
+      <nav className='flex w-full items-center justify-between'>
         <Logo className='w-36 md:w-40 xl:w-48' />
         <Link
           href='/'
-          className='flex items-center gap-1 text-static-black underline text-xs md:text-base font-normal cursor-pointer'
+          className='text-strong-950 cursor-pointer text-xs font-normal underline underline-offset-2 md:text-base'
         >
           Claim Ticket
         </Link>
