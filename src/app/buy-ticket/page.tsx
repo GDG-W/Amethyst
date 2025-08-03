eff497aa3f659d074b83666fe88cd72cb8f8bf30
DrPrime01 <drprime.dev@gmail.com>
2025-08-03 19:49:33 +0100

    fix: added the missing props to the breadcrumb component in buy-ticket page

--- src/app/buy-ticket/page.tsx
+++ src/app/buy-ticket/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div>
       <h1>Hello</h1>
-      <Breadcrumb breadcrumbList={breadcrumbList} />
+      <Breadcrumb breadcrumbList={breadcrumbList} activeIndex={2} handleClick={() => {}} />
     </div>
   );
 }
