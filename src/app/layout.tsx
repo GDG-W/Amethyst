8988de993f5190fbe9bbfda91b6c4e5e2b41ac2a
Oluwatobiju Judah <tobijudah@gmail.com>
2025-08-04 11:46:34 +0100

    refactor: simplify base layout by removing static content and retaining children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
 import { Inter } from "next/font/google";
-import Link from "next/link";
-
-import Header from "@/components/layout/header";
 
 import "./globals.css";
 import type { Metadata } from "next";
@@ -27,19 +24,7 @@ export default function RootLayout({
         style={{ backgroundImage: "url('/bg.png')" }}
         className={`${inter.variable} md:rounded-2.5xl min-h-dvh bg-cover bg-no-repeat px-5 antialiased md:min-h-[95dvh]`}
       >
-        <div className='mx-auto max-w-4xl pt-8 md:pt-14'>
-          <Header />
-          <h1 className='heading-5 md:heading-4 mt-11 mb-2 font-semibold md:mt-17 md:mb-5'>
-            Be a part of the <br /> DevFest 2025 physically!
-          </h1>
-          <p className='text-soft-400 text-xs'>
-            <span>Already have a ticket? </span>
-            <Link href='/' className='text-sub-600 underline underline-offset-2'>
-              Claim Ticket
-            </Link>
-          </p>
-          {children}
-        </div>
+        <div className='mx-auto max-w-4xl pt-8 md:pt-14'>{children}</div>
       </body>
     </html>
   );
