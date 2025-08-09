887be735555180e5a4bef76b7f97248525c17b04
Oluwatobiju Judah <tobijudah@gmail.com>
2025-08-09 16:47:47 +0100

    feat: add getTickets function to fetch tickets by type from API

--- src/services/ticket.service.ts
+++ src/services/ticket.service.ts
@@ -1,1 +1,10 @@
-// Domain-specific API functions
+import API from "@/services/api";
+import { Ticket, TicketType } from "@/types/ticket";
+
+export const getTickets = async (ticketType: TicketType) => {
+  return API.get<Ticket[]>(`/api/tickets?ticket_type=${ticketType}`);
+};
+
+export default {
+  getTickets,
+};
