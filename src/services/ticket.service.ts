61272954ae28afe252e792d257db7836b107ba93
Oluwatobiju Judah <tobijudah@gmail.com>
2025-08-10 13:56:42 +0100

    feat: updates get tickets path

--- src/services/ticket.service.ts
+++ src/services/ticket.service.ts
@@ -2,7 +2,7 @@ import API from "@/services/api";
 import { Ticket, TicketType } from "@/types/ticket";
 
 export const getTickets = async (ticketType: TicketType) => {
-  return API.get<Ticket[]>(`/api/tickets?ticket_type=${ticketType}`);
+  return API.get<Ticket[]>(`/tickets?ticket_type=${ticketType}`);
 };
 
 export default {
