e6345e3415cb9bc9fe80aecad14b7902f5b308ef
Emmanuel Olubiyi <grggmmnl@gmail.com>
2025-08-15 22:10:55 +0100

    fix: csr bailout issue

--- src/app/claim/page.tsx
+++ src/app/claim/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
 import ClaimTicketForm from "@/components/form/claim-ticket.form";
@@ -48,20 +48,22 @@ export default function ClaimPage() {
   };
 
   return (
-    <div className="relative mx-auto mt-7 w-full max-w-[450px] pb-[15.813rem] md:mt-18 md:pb-18">
-      <h1 className="text-center font-[inter] text-[2rem] leading-tight font-medium tracking-tight">
-        Finish your registration to claim your ticket
-      </h1>
+    <Suspense>
+      <div className="relative mx-auto mt-7 w-full max-w-[450px] pb-[15.813rem] md:mt-18 md:pb-18">
+        <h1 className="text-center font-[inter] text-[2rem] leading-tight font-medium tracking-tight">
+          Finish your registration to claim your ticket
+        </h1>
 
-      <ClaimTicketForm token={ticketToken} formCallbackFn={openModalAndWait} />
+        <ClaimTicketForm token={ticketToken} formCallbackFn={openModalAndWait} />
 
-      <Modal isOpen={showModal}>
-        <SuccessCard
-          title="Registration Successful"
-          summary="You have successfully registered for Devfest Lagos 2025. Check your email for your Ticket ID"
-          onClose={closeModalCb}
-        />
-      </Modal>
-    </div>
+        <Modal isOpen={showModal}>
+          <SuccessCard
+            title="Registration Successful"
+            summary="You have successfully registered for Devfest Lagos 2025. Check your email for your Ticket ID"
+            onClose={closeModalCb}
+          />
+        </Modal>
+      </div>
+    </Suspense>
   );
 }
