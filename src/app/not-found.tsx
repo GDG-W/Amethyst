aa8d6a2a33c7c8bb06c9471d8f56b7bd7e524591
fasina-ayomikun <ayomikunfasina@yahoo.com>
2025-08-15 05:45:23 +0100

    feat: added the fonts folder and resized the eye icon

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,18 +3,27 @@ import React from "react";
 
 import { useRouter } from "next/navigation";
 
+import localFont from "next/font/local";
+
 import { EyesIcon } from "@/components/icons/eyes";
 import Button from "@/components/ui/button";
 import Header from "@/components/layout/header";
+
+const akira = localFont({
+  src: "../components/fonts/Akira-Expanded-Demo.otf",
+  display: "swap",
+});
 
 const NotFound = () => {
   const navigator = useRouter();
   return (
     <section>
       <Header />
-      <div className="max-width-5xl mx-auto mt-15">
-        <EyesIcon className="mx-auto w-full max-w-[330px]" />
-        <h3 className="text-strong-950 my-5 text-center font-[Akira_Expanded] text-4xl font-black uppercase sm:text-5xl">
+      <div className="max-width-5xl mx-auto my-15">
+        <EyesIcon className="mx-auto w-full max-w-xl" />
+        <h3
+          className={`text-strong-950 my-7 text-center sm:text-4xl ${akira.className} text-2xl uppercase`}
+        >
           404, Page not found.
         </h3>
         <div className="mx-auto mt-10 w-full max-w-[330px]">
