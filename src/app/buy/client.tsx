57ac19a996fcf2c542668ee82b9c17dcde33e164
TemitopeAsama <temitopeasama@gmail.com>
2025-08-16 20:42:05 +0100

    chore: refactored state management using zustand

--- src/app/buy/client.tsx
+++ src/app/buy/client.tsx
@@ -2,14 +2,12 @@
 import { ChevronLeft } from "lucide-react";
 import React, { useState } from "react";
 
-import { useQueryClient } from "@tanstack/react-query";
-
 import BuyTicket from "@/components/steps/buy-ticket";
 import Breadcrumb from "@/components/ui/breadcrumb";
 import OrderSummary from "@/components/ui/order-summary";
 import Button from "@/components/ui/button";
-import AttendeeInfo from "@/components/form/attendee-info";
 import BuyerInformation from "@/components/form/buyer-info";
+import { useBuyFormStore } from "@/store/buy-form-store";
 
 const steps = ["Buy Ticket", "Buyer Information"];
 
@@ -32,15 +30,11 @@ export type AttendeeInfo = {
 };
 
 export default function BuyPageClient() {
-  const queryClient = useQueryClient();
   const [step, setStep] = useState(0);
-  const [orderItems, setOrderItems] = useState<OrderItem[]>([]);
-  const [buyerInfo, setBuyerInfo] = useState<BuyerInfo | null>(null);
-  const [attendeeInfo, setAttendeeInfo] = useState<AttendeeInfo | null>(null);
+  const { orderItems, buyerInfo, attendeeInfo } = useBuyFormStore();
 
   const handleContinue = () => {
     if (orderItems.length < 1) return;
-    queryClient.setQueryData(["orderItems"], orderItems);
     setStep((s) => Math.min(s + 1, steps.length - 1));
     return true;
   };
@@ -49,30 +43,13 @@ export default function BuyPageClient() {
     if (step > 0) setStep(step - 1);
   };
 
-  const handleBuyerSubmit = (buyer: BuyerInfo, attendees: AttendeeInfo): boolean => {
-    console.log({ buyer, attendees });
-    if (!buyer || !attendees) return false;
-
-    setBuyerInfo(buyer);
-    setAttendeeInfo(attendees);
-
-    queryClient.setQueryData(["buyerInfo"], buyer);
-    queryClient.setQueryData(["attendeeInfo"], attendees);
-
-    console.log("Buyer Info:", buyer);
-    console.log("Attendee Info:", attendees);
-
-    setStep((s) => Math.min(s + 1, steps.length - 1));
-    return true;
-  };
-
   const handleNext = () => {
     if (step === 0) {
       return handleContinue();
     } else if (step === 1) {
       console.log({ buyerInfo, attendeeInfo });
       if (buyerInfo && attendeeInfo) {
-        return handleBuyerSubmit(buyerInfo, attendeeInfo);
+        console.log("PROCESS PAYMENT");
       }
       return false;
     }
@@ -95,10 +72,8 @@ export default function BuyPageClient() {
       </div>
       <div className="flex flex-col gap-[20px] pt-5 sm:flex-row">
         <div className="w-full sm:flex-[9]">
-          {step === 0 && <BuyTicket onItemsChange={setOrderItems} />}
-          {step === 1 && (
-            <BuyerInformation selectedDates={orderItems} onSubmit={handleBuyerSubmit} />
-          )}
+          {step === 0 && <BuyTicket />}
+          {step === 1 && <BuyerInformation selectedDates={orderItems} />}
           <Button onClick={handleNext} className="mt-10 sm:hidden" disabled={orderItems.length < 1}>
             {step === steps.length - 1 ? "Proceed to Pay" : "Continue"}
           </Button>
