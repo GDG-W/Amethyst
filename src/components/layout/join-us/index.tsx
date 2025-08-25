a6a88319d63c2d65ba8531f315c3a064cb2093e8
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-08-25 16:12:10 -0400

    fix: adjust padding and layout for Join Us section on larger screens

--- src/components/layout/join-us/index.tsx
+++ src/components/layout/join-us/index.tsx
@@ -7,15 +7,15 @@ import Button from "@/components/ui/home-button";
 const JoinUs = () => {
   return (
     <div className="flex justify-center bg-[#FCF6DF] px-3 py-3 md:px-36 md:py-4">
-      <div className="flex w-full flex-col items-center justify-center space-y-5 rounded-sm bg-[#171717] p-5 text-white md:space-y-7 md:rounded-xl md:p-18">
+      <div className="flex w-full flex-col items-center justify-center space-y-5 rounded-sm bg-[#171717] p-5 text-white md:space-y-7 md:p-8 lg:rounded-xl lg:p-18">
         <h1 className="font-akira w-full text-center text-base font-bold uppercase md:text-2xl">
           The future is <br /> technology, join us at <br />
           <span className="text-[#F6B51E]">DevFest Lagos 2025</span> <br /> and help shape that{" "}
           <br /> future in Nigeria
         </h1>
 
-        <Button className="w-full text-sm text-[#141414] md:mx-0 md:text-base">Buy Tickets</Button>
-        <div className="relative hidden w-full md:block">
+        <Button className="w-full text-sm text-[#141414] md:text-base">Buy Tickets</Button>
+        <div className="relative hidden w-full lg:block">
           <Image
             src="/see-you-there.svg"
             alt="See you there"
