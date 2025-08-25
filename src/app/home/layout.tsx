a6dfa4b9e5eef414340c5573d0a1757bf9fca2f6
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-08-25 16:08:04 -0400

    feat: add Join Us section to the homepage layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,9 +1,12 @@
+import JoinUs from "@/components/layout/join-us";
+
 import BuySquadTickets from "../../components/layout/BuySquadTickets.tsx/index";
 
 export default function Homelayout() {
   return (
     <div>
       <BuySquadTickets />
+      <JoinUs />
     </div>
   );
 }
