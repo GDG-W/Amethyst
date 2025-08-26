095c8ad48f133c21e8491381718e41a229c1a02a
unknown <eazyisreal192@gmail.com>
2025-08-26 18:59:14 +0100

    chore: built recap section

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,8 +1,8 @@
 import JoinUs from "@/components/layout/join-us";
 
 import BuySquadTickets from "../../components/layout/squad-tickets/index";
-import FAQ from "../../components/layout/faq/index";
-import Footer from "../../components/layout/footer/index";
+import FAQ from "../../components/layout/FAQ/index";
+import Footer from "../../components/layout/Footer/index";
 import Recap from "../../components/layout/recap/index";
 
 export default function Homelayout() {
