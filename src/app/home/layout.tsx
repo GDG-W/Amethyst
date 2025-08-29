730be8f2cd953a557f4019180aaa8fd72de87103
unknown <eazyisreal192@gmail.com>
2025-08-29 16:59:43 +0100

    chore: sticky note section

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -3,11 +3,15 @@ import JoinUs from "@/components/layout/join-us";
 import BuySquadTickets from "../../components/layout/squad-tickets/index";
 import FAQ from "../../components/layout/FAQ/index";
 import Footer from "../../components/layout/Footer/index";
+import MobileStickyNote from "../../components/layout/sticky-notes/mobile";
 import Recap from "../../components/layout/recap/index";
+import StickyNote from "../../components/layout/sticky-notes/index";
 
 export default function Homelayout() {
   return (
     <div>
+      <StickyNote />
+      <MobileStickyNote />
       <BuySquadTickets />
       <FAQ />
       <Recap />
