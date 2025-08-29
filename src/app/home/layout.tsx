f41c7ec6a3aa9d1a7a53a4351bfddac513212b39
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-08-29 08:51:01 -0400

    feat: add Hero component to homepage layout and include DevFest 2025 SVG

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,10 +1,13 @@
 import JoinUs from "@/components/layout/join-us";
 
+import Hero from "@/components/layout/hero";
+
 import BuySquadTickets from "../../components/layout/BuySquadTickets.tsx/index";
 
 export default function Homelayout() {
   return (
     <div>
+      <Hero />
       <BuySquadTickets />
       <JoinUs />
     </div>
