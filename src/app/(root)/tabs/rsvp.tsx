f2dacf0a8ef8a3c74b00cb375bed625b1fafd5a6
DrPrime01 <drprime.dev@gmail.com>
2025-09-02 02:42:23 +0100

    feat: created the dashboard tabs components

--- src/app/(root)/tabs/rsvp.tsx
+++ src/app/(root)/tabs/rsvp.tsx
@@ -1,8 +1,8 @@
 export default function RSVP() {
   return (
     <div className="flex flex-col gap-1 text-center md:justify-center">
-      <h3 className="heading-3">Welcome, Human</h3>
-      <p className="label-2">Reserve your seat for sessions you don’t want to miss.</p>
+      <h3 className="heading-3 text-strong-950 font-medium">Welcome, Human</h3>
+      <p className="label-2 text-sub-600">Reserve your seat for sessions you don’t want to miss.</p>
     </div>
   );
 }
